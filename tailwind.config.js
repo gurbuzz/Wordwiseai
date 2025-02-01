e613c48c8b7f2e8d0f4e3c5d3a6a58509aaabee5
gurbuzz <atacan.sss1@gmail.com>
2025-02-01 17:49:50 +0300

    Added Game play

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#F5F5F5', 
+      },
+    },
   },
   plugins: [],
-}
+};
